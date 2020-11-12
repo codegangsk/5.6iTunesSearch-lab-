93fbe9fe39f548018d8b3a18640ddb1275a01f51
codegangsk <kjhj0604@gmail.com>
2020-11-12 15:23:46 +0900

    create StoreItemController class and copy and paste fetchItems function from playground

---
+++ iTunesSearch/StoreItemController.swift
@@ -0,0 +1,43 @@
+//
+//  StoreItemController.swift
+//  iTunesSearch
+//
+//  Created by Sophie Kim on 2020/11/12.
+//  Copyright © 2020 Caleb Hicks. All rights reserved.
+//
+
+import Foundation
+
+class StoreItemControllerClass {
+    func fetchItems(matching query: [String: String], completion: @escaping ([StoreItem]?) -> Void) {
+        let baseURL = URL(string: "https://itunes.apple.com/search?")!
+        
+        guard let url = baseURL.withQueries(query) else {
+            completion(nil)
+            print("Unable to build URL with supplied queries.")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data {
+                if let json = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers),
+                   let jsonData = try? JSONSerialization.data(withJSONObject: json, options: .prettyPrinted) {
+                    print(String(decoding: jsonData, as: UTF8.self))
+                } else {
+                    print("json data malformed")
+                }
+            }
+            
+            guard let data = data,
+                  let storeItems = try? JSONDecoder().decode(StoreItems.self, from: data)
+            else {
+                print("Either no data was returned, or data was not serialized.")
+                completion(nil)
+                return
+            }
+
+            completion(storeItems.results)
+        }
+        task.resume()
+    }
+}
